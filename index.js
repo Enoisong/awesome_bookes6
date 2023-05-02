06775b8c6ca7d901de291596482d9bb21dc7f59d
Enoisong <enobongitiaba988@gmail.com>
2023-05-02 14:46:39 -0700

    edit  modules

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-// Importing needed js files
 import Book from './modules/book.js';
 import formInputs from './modules/form.js';
 import app from'./modules/app.js';
 import { DateTime } from './modules/luxon.js';
 
-// Calling needed functions from the js modules
 formInputs();
 Book.displayBooks();
 app();
 
-// Populating the Date
+// function to Populate the Date
 const populateDate = () => {
     const date = document.getElementById('date');
     date.innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
